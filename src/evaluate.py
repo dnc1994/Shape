06cf5c60cb0b88b354088899b99552b0f0291243
Linghao Zhang <zlhdnc1994@gmail.com>
2015-12-13 13:06:31 +0800

    evaluation done

--- src/evaluate.py
+++ src/evaluate.py
@@ -14,21 +14,22 @@ def eval_dataset(input_dir, output_file, need_latex=True):
             confusion_matrix[shape][label] = 0
     error_list = []
 
-    for filename in os.listdir(input_dir):
-        if not filename.endswith('.png'):
-            continue
-        print 'Recognizing {0}'.format(filename)
-        index, label = filename.split('.')[0].split('_')
-        input_file = os.path.join(input_dir, filename)
-        try:
-            recognized_label = recognizer.recognize(input_file)
-        except:
-            recognized_label = 'FAIL'
-        label = label.title()
-        recognized_label = recognized_label.title()
-        if label != recognized_label:
-            error_list.append((filename, label, recognized_label))
-        confusion_matrix[label][recognized_label] = 1 + confusion_matrix[label].get(recognized_label, 0)
+    for (root, subdirs, filenames) in os.walk(input_dir):
+        for filename in filenames:
+            if not filename.endswith('.png'):
+                continue
+            print 'Recognizing {0}'.format(filename)
+            index, label = filename.split('.')[0].split('_')
+            input_file = os.path.join(root, filename)
+            try:
+                recognized_label = recognizer.recognize(input_file)
+            except:
+                recognized_label = 'FAIL'
+            label = label.title()
+            recognized_label = recognized_label.title()
+            if label != recognized_label:
+                error_list.append((filename, label, recognized_label))
+            confusion_matrix[label][recognized_label] = 1 + confusion_matrix[label].get(recognized_label, 0)
 
     confusion_matrix_percent = copy.deepcopy(confusion_matrix)
     for shape in shape_list:
@@ -63,4 +64,4 @@ def eval_dataset(input_dir, output_file, need_latex=True):
 
 if __name__ == '__main__':
     # eval_dataset(sketch_image_dir, 'C:\\Home\\Projects\\Shape\\data\\eval_test_image', True)
-    eval_dataset(train_image_dir, 'C:\\Home\\Projects\\Shape\\data\\eval_test_image')
+    eval_dataset(test_image_dir, 'C:\\Home\\Projects\\Shape\\data\\eval_test_image')
--- src/recognize.py
+++ src/recognize.py
@@ -11,12 +11,11 @@ class ShapeRecognizer(object):
         fsets = {}
         rules = {}
 
-        fsets['No_Corners'] = fset.LeftSkewTrapezoid('No_Corners', (0, 0), (2, 1), (4, 0))
-        fsets['Circle_Like_Thinness'] = fset.Triangle('Circle_Like_Thinness', (12.5, 0), (14, 1), (15, 0))
-        fsets['Square_Like_Thinness'] = fset.Triangle('Square_Like_Thinness', (15, 0), (16, 1), (20, 0))
-        fsets['Triangle_Like_Extent'] = fset.Trapezoid('Triangle_Like_Extent', (0.4, 0), (0.45, 1), (0.55, 1), (0.6, 0))
-        fsets['Ellipse_Like_Extent'] = fset.Triangle('Ellipse_Like_Extent', (0.7, 0), (0.8, 1), (0.85, 0))
-        fsets['Rectangle_Like_Extent'] = fset.RightSkewTrapezoid('Rectangle_Like_Extent', (0.85, 0), (0.95, 1), (1, 0))
+        fsets['Circle_Like_Thinness'] = fset.Trapezoid('Circle_Like_Thinness', (13.99, 0), (15.07, 1), (15.44, 1), (16.39, 0))
+        fsets['Square_Like_Thinness'] = fset.Trapezoid('Square_Like_Thinness', (15.44, 0), (15.87, 1), (17.04, 1), (18.72, 0))
+        fsets['Triangle_Like_Extent'] = fset.Triangle('Triangle_Like_Extent', (0.4828, 0), (0.5170, 1), (0.5512, 0))
+        fsets['Ellipse_Like_Extent'] = fset.Trapezoid('Ellipse_Like_Extent', (0.7450, 0), (0.7740, 1), (0.790, 1), (0.8180, 0))
+        fsets['Rectangle_Like_Extent'] = fset.RightSkewTrapezoid('Rectangle_Like_Extent', (0.795, 0), (0.9377, 1), (1, 0))
 
 
         rules['Circle'] = 'IF Thinness IS Circle_Like_Thinness AND Extent IS Ellipse_Like_Extent THEN Shape IS Circle'
